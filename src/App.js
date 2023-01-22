9b58029e06cf0b9041198636adf706e1409283fa
dhruv <pd86452@gmail.com>
2023-01-22 23:11:50 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import Sidebar from './components/Sidebar';
+import Main from './components/Main';
+
+function App() {
+  return (
+    <div className="App">
+    <div className='app_body'>
+      <Sidebar/>
+      <Main />
+    </div>
+     
+    </div>
+  );
+}
+
+export default App;
