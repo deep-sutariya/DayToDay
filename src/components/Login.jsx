b91e52374feec364e60a11ca2df2264fc26da709
pd86452 <pd86452@gmail.com>
2023-02-12 16:30:16 +0530

    abc

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -37,8 +37,9 @@ function Login() {
   return (
     <div className='login'>
 
-        <h1>login</h1>
+  
         <form action="POST">
+        
             <input type="email" onChange={(e)=>{setEmail(e.target.value)}} placeholder="email" name="" id=""/>
             <input type="password" onChange={(e)=>{setPassword(e.target.value)}} placeholder="password" name="" id=""/>
 
